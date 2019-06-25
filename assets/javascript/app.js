f50f450ac99b4e5b332f8942e11e2b449590769d
Mafalda <mafalda.villazan@gmail.com>
2019-06-25 16:11:16 -0700

    Adding buttoms to the page

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+//Add the information of bootstrap/ Jquery and API
+//Code of Api for FILMS of adding buttons follow the same path
+//Get first button to work
+// Add the adding button with the categories needed
