c1d9cae4479cb9b6f1cd91031e605a9bbb70168b
Mafalda <mafalda.villazan@gmail.com>
2019-06-25 17:12:05 -0700

    Adding a for loob so it captures id from HTML and creates a url with that particular search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,13 @@
 //Can we make the code better with when the user clicks on any button take that info
 
 //MOON
-$("#moon-button").on("click", function () {
+$("button").on("click", function () {
+    //Taking the value giving to that button in the HTML
+    var typeOfGif = $(this).attr("data-science")
+    console.log(typeOfGif);
     //Add the link needed for calling the API
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=moon";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    typeOfGif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10"
 
     //Using Jquery to call the Apo
     $.ajax({
@@ -17,18 +21,30 @@ $("#moon-button").on("click", function () {
         method: "GET"
     })
         .then(function (response) {
+            //console.log to see the result of the object
             console.log(response)
 
-            //taking from that response just what we want
-            var imageUrl = response.data.fixed_width_small_url;
-            // creates an 
-            var moonImage = $("<img>");
+            //Create a a variable of the results that will look into the data
+            var results = response.data;
 
-            // we are setting src attribute to the image we are looking for
-            moonImage.attr("src", imageUrl);
+            //Create a For loop that will create results
+            for (var i = 0; i < results.length; i++) {
+                //Create a div that will contain the gif
+                var gifDiv = $("<div class=col-1>");
+                //Show the rating of the gif
+                var rating = results[i].rating;
+                //Add the result or value of the rating in our HTML 
+                var ratingText = $("<p>").text("Rating: " + rating);
 
-            // adding the image to the div image
-            $("#images").prepend(moonImage);
+                //Create and image that has the source of that particular url
+                var gifImg = $("<img>");
+                gifImg.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(ratingText);
+                gifDiv.prepend(gifImg);
+
+                $("#images").prepend(gifDiv);
+            }
         });
 
 })
