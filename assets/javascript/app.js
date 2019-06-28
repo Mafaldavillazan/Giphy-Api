232e144499de8c43df681e347698b4e106ccaaff
Mafalda <mafalda.villazan@gmail.com>
2019-06-27 17:08:06 -0700

    Changing the Layout where the gifs are displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,107 @@
+$(document).ready(function () {
 
-//TO DOS
-//
-
-//when the document is ready...
-
+    //TO DOS
+    //Improve the style
 
 
-//USER INPUTS
+    //USER INPUTS
+    //create an array to store the user input
+    var arrUserGif = []
 
 
-//create an array to store the user input
-var arrUserGif = []
-console.log(arrUserGif)
+    //BUTTONS PRELOAD
+    //When any of the buttons preload is clicked ...
 
+    $(".definedButton").on("click", function () {
 
-//BUTTONS PRELOAD
-//When any of the buttons preload is clicked ...
+        //Taking the value giving to that button in the HTML
+        var typeOfGif = $(this).attr("data-science")
+    
+        //Add the link needed for calling the API
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            typeOfGif + "&api_key=EGOEsx7oEvGNmXEIBBfiQzAhbKu7dngF&limit=4"
 
-$(".definedButton").on("click", function () {
+        //Clear the results
+        //Make the user Input its data
 
-    //Taking the value giving to that button in the HTML
-    var typeOfGif = $(this).attr("data-science")
-    console.log(typeOfGif)
-    //Add the link needed for calling the API
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        typeOfGif + "&api_key=EGOEsx7oEvGNmXEIBBfiQzAhbKu7dngF&limit=10"
+        //Using Jquery to call the Api
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                //console.log to see the result of the object
+                console.log(response)
 
-    //Clear the results
-    //Make the user Input its data
+                //Create a a variable of the results that will look into the data
+                var results = response.data;
 
-    //Using Jquery to call the Api
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+                //Create a For loop that will create results
+                for (var i = 0; i < results.length; i++) {
+                    //Create a div that will contain the gif
+                    var gifDiv = $("<div class=col-2>");
+                    //Show the rating of the gif
+                    var rating = results[i].rating;
+                    //Add the result or value of the rating in our HTML 
+                    var ratingText = $("<h5 class=card-text allign>").text("Rating: " + rating);
+                    var ratingText = $("<p>").text("Rating: " + rating);
+
+                    //Create and image that has the source of that particular url
+                    var gifImg = $("<img>");
+                    gifImg.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(gifImg);
+                    gifDiv.prepend(ratingText);
+
+                    $("#images").append(gifDiv);
+                }
+            });
+
     })
-        .then(function (response) {
-            //console.log to see the result of the object
-            console.log(response)
 
-            //Create a a variable of the results that will look into the data
+
+    //USER BUTTONS
+
+    //Export the data the user types and add it to the new button
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
+
+        // Grab the input from the form
+        var userGifText = $("#gif-input").val().trim();
+        console.log(userGifText)
+        arrUserGif.push(userGifText);
+
+        //Create user button
+        var $userButton = $("<button>");
+        $userButton.addClass("gifButton");
+        $userButton.attr("data-gif", userGifText);
+        $userButton.text(userGifText);
+        $("#userButtons").append($userButton);
+
+    })
+
+    //
+    function displayGIF() {
+        console.log(this)
+        var userGif = $(this).attr("data-gif");
+        console.log(this)
+        var queryURLuser = "https://api.giphy.com/v1/gifs/search?q=" +
+        userGif + "&api_key=EGOEsx7oEvGNmXEIBBfiQzAhbKu7dngF&limit=4";
+        //Using Jquery to call the Api
+        $.ajax({
+            url: queryURLuser,
+            method: "GET"
+        }).then(function (response) {
+            console.log(results)
+
             var results = response.data;
 
+            //Clear the document when loaded
+            $(".gifDisplay").remove()
             //Create a For loop that will create results
             for (var i = 0; i < results.length; i++) {
                 //Create a div that will contain the gif
-                var gifDiv = $("<div class=col-3>");
+                var gifDivUser = $("<div class=col-2 gifDisplay>");
                 //Show the rating of the gif
                 var rating = results[i].rating;
                 //Add the result or value of the rating in our HTML 
@@ -54,79 +111,17 @@ $(".definedButton").on("click", function () {
                 var gifImg = $("<img>");
                 gifImg.attr("src", results[i].images.fixed_height.url);
 
-                gifDiv.prepend(ratingText);
-                gifDiv.prepend(gifImg);
+                gifDivUser.prepend(ratingText);
+                gifDivUser.prepend(gifImg);
 
-                $("#images").prepend(gifDiv);
+                $("#images").prepend(gifDivUser);
             }
-        });
+        })
+    }
 
-})
-
-
-//USER BUTTONS
-//Create a function that displays the user info in a button
+    $(document).on("click", ".gifButton", displayGIF);
 
 
-//Create a function that will display the gifs
-function displayUserGif() {
+});
 
-}
 
-//Export the data the user types and add it to the new button
-$("#add-gif").on("click", function (event) {
-    event.preventDefault();
-
-    // Grab the input from the form
-    var userGifText = $("#gif-input").val().trim();
-    console.log(userGifText)
-    arrUserGif.push(userGifText);
-
-    //Create user button
-    var $userButton = $("<button>");
-    $userButton.addClass("gifButton");
-    $userButton.attr("data-gif", userGifText);
-    $userButton.text(userGifText);
-    $("#userButtons").append($userButton);
-
-    //We search and display the gif
-
-})
-
-//
-function displayGIF() {
-    console.log(this)
-    var userGif = $(this).attr("data-gif");
-    console.log(this)
-    var queryURLuser = "https://api.giphy.com/v1/gifs/search?q=" +
-        userGif + "&api_key=EGOEsx7oEvGNmXEIBBfiQzAhbKu7dngF&limit=10";
-    //Using Jquery to call the Api
-    $.ajax({
-        url: queryURLuser,
-        method: "GET"
-    }).then(function (response) {
-        console.log(results)
-
-        var results = response.data;
-        //Create a For loop that will create results
-        for (var i = 0; i < results.length; i++) {
-            //Create a div that will contain the gif
-            var gifDivUser = $("<div class=col-3>");
-            //Show the rating of the gif
-            var rating = results[i].rating;
-            //Add the result or value of the rating in our HTML 
-            var ratingText = $("<p>").text("Rating: " + rating);
-
-            //Create and image that has the source of that particular url
-            var gifImg = $("<img>");
-            gifImg.attr("src", results[i].images.fixed_height.url);
-
-            gifDivUser.prepend(ratingText);
-            gifDivUser.prepend(gifImg);
-
-            $("#images").prepend(gifDivUser);
-        }
-    })
-}
-
-$(document).on("click", ".gifButton", displayGIF);
